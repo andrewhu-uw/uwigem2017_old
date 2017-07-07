75960dd7c7e7901b580450b93ff4824a329face6
Andrew <andrewhu@uw.edu>
2017-07-06 22:24:52 -0700

    Added git guide
    Reference sheet so that I don't have to go googling everywhere for repeat Git questions. Firmware downgrade script now has multiline comments

--- raspbian/firmware_downgrade.sh
+++ raspbian/firmware_downgrade.sh
@@ -1,9 +1,9 @@
 #@author Andrew Hu @date 7/6/2017
 
-#Some people have encountered a problem where Pi4J will talk to wiringPi, and wiringPi will give out
-#a firmware error, saying that it cannot identify the hardware BCM2835 as a Raspberry Pi, even though
-#it clearly is a Pi. This can be solved by downgrading the kernel to the latest version that worked
-#which is 4.4.49
+:'Some people have encountered a problem where Pi4J will talk to wiringPi, and wiringPi will give out
+a firmware error, saying that it cannot identify the hardware BCM2835 as a Raspberry Pi, even though
+it clearly is a Pi. This can be solved by downgrading the kernel to the latest version that worked
+which is 4.4.49'
 
 #The long string is the git hash for a commit to this git repo that archives the kernel
 #https://github.com/Hexxeh/rpi-firmware/commits/master
